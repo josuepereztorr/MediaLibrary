bfd3318234f12257abd41ed8367963e77218b75a
Josue <josuepereztorr@gmail.com>
2022-03-06 19:35:17 -0600

    Created the Media class

---
+++ Media.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace MediaLibrary
+{
+    public class Media
+    {
+        // public properties
+        public UInt64 MediaId { get; set; }
+        public string Title { get; set; }
+        public List<string> Genres { get; set; }
+
+        // constructor
+        public Media()
+        {
+            Genres = new List<string>();
+        }
+
+        // public method
+        public string Display()
+        {
+            return $"Id: {MediaId}\nTitle: {Title}\nGenres: {string.Join(", ", Genres)}\n";
+        }
+    }
+}
