c83677296eb50d250b3edfb38c6aa07e835c2d27
Josue <josuepereztorr@gmail.com>
2022-03-06 20:58:45 -0600

    Created FileScrubber class

--- Program.cs
+++ Program.cs
@@ -10,40 +10,10 @@ namespace MediaLibrary
         static void Main(string[] args)
         {
             Logger.Info("Program Started");
-
-            Movie movie = new Movie
-            {
-                MediaId = 123,
-                Title = "Greatest Movie Ever, The (2020)",
-                Genres = {"Comedy", "Romance"},
-                Director = "Jeff Grissom",
-                RunningTime = new TimeSpan(2, 21, 23),
-            };
-
-            movie.Display();
             
-            Album album = new Album
-            {
-                MediaId = 321,
-                Title = "Greatest Album Ever, The (2020)",
-                Artist = "Jeff's Awesome Band",
-                RecordLabel = "Universal Music Group",
-                Genres = { "Rock" }
-            };
-            Console.WriteLine(album.Display());
+            string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+            Logger.Info(scrubbedFile);
             
-            Book book = new Book
-            {
-                MediaId = 111,
-                Title = "Super Cool Book",
-                Author = "Jeff Grissom",
-                PageCount = 101,
-                Publisher = "",
-                Genres = { "Suspense", "Mystery" }
-            };
-            Console.WriteLine(book.Display());
-
-
             Logger.Info("Program Ended");
         }
     }
