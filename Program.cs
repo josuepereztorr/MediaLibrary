b294a6310b4e055346d8b1572456430867c89258
Josue <josuepereztorr@gmail.com>
2022-03-06 19:28:01 -0600

    Installed and Configured Nlog

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.IO;
+using NLog;
 
 namespace MediaLibrary
 {
     class Program
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
